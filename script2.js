82ff288fd6be2bb8e738eb5481eb87d9bb917e8c
Musharruf <mdmusharrufbinjafar13@gmail.com>
2022-11-13 23:53:21 +0530

    new js file added

---
+++ script2.js
@@ -0,0 +1,8 @@
+// // this function does not works if the variable is declared with var keyword
+// (function(){
+// let msg = "Hi i am musharruf";
+// function greet(){
+//     console.log(msg);
+// }
+// greet();
+// })();
