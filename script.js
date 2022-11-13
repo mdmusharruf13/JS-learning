4a9ef5878e3fcdff2b4f2a558d3d1415aa244835
Musharruf <mdmusharrufbinjafar13@gmail.com>
2022-11-13 23:54:53 +0530

    script file is updated

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-alert("hi i am musharruf");
-document.write("welcome to JavaScript Learning");
+// alert("hi i am musharruf");
+document.write("welcome to JavaScript Learning <br>");
 // prompt("are you ready ? ");
-console.log(598);
+// this is single line comment
+/*this is multiple 
+line comment*/
+console.log("My number is "+598);
+document.write("// - single line comment<br>");
+document.write("/*content*/ - this is multiple line comment<br>");
+console.log(590+8);
+var num1 = 12;
+var num2 = 13;
+var num3 = num1+num2;
+console.log(num3);
+var Myname = "musharruf";
+console.log(Myname);
+// operators in javascript
+var a=5;
+var b=10;
+document.write("a=5 and b=10 <br>")
+document.write("Addition of a and b is :"+(a+b));
+document.write("<br> Subtraction of a and b is :"+(a-b));
+document.write("<br> Multiplication of a and b is :"+(a*b));
+document.write("<br> Division of a and b is :"+(a/b));
+var powerr = 5**2;
+document.write("<br> 5 power 2 is :"+powerr);
+document.write("<br>"+"a="+a+"<br>");
+a++;
+document.write("a="+a+"<br>");
+document.write(typeof(10)+"<br>");
+document.write(typeof(musharruf)+"<br>");
+document.write(typeof("musharruf")+"<br>");
+document.write(`i'm ${Myname}`);
